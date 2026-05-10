788dafd215b349dcd21ff40b40fd54ba5b656229
YusufAsim <yusufasimdemirhan@gmail.com>
2026-05-10 21:22:33 +0300

    feat: implement word replacement and sentence vectors

--- part1_solution.py
+++ part1_solution.py
@@ -10,16 +10,43 @@ def replace_with_similar(sentence, indices):
     """
     Replace the tokens at the given indices with their top-1 most similar words.
     """
-    # TODO: Implement
-    pass
+    tokens = sentence.split()
+    replaced_tokens = tokens.copy()
+    similar_dict = {}
+
+    for index in indices:
+        word = tokens[index]
+        top_similar = model.most_similar(word, topn=5)
+        similar_dict[word] = top_similar
+        replaced_tokens[index] = top_similar[0][0]
+
+    new_sentence = " ".join(replaced_tokens)
+    return new_sentence, similar_dict
 
 
 def sentence_vector(sentence):
     """
     Compute sentence embedding as mean of word vectors.
     """
-    # TODO: Implement
-    pass
+    tokens = sentence.split()
+    vector_dict = {}
+    word_vectors = []
+
+    for token in tokens:
+        if token in model:
+            vector = model[token]
+        else:
+            vector = np.zeros(EMBEDDING_DIM)
+
+        vector_dict[token] = vector
+        word_vectors.append(vector)
+
+    if len(word_vectors) == 0:
+        sentence_vec = np.zeros(EMBEDDING_DIM)
+    else:
+        sentence_vec = np.sum(word_vectors, axis=0) / len(word_vectors)
+
+    return vector_dict, sentence_vec
 
 
 def most_similar_sentences(file_path, query):
